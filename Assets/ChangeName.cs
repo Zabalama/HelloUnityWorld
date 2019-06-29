fc3d912b5c68d92a03fece81412f8d4f07fb6ef2
Zabalama <47012222+Zabalama@users.noreply.github.com>
2019-06-29 02:16:39 -0400

    Firt commit

---
+++ Assets/ChangeName.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class ChangeName: MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+        TextMeshProUGUI textmeshProUGUI = GetComponent<TextMeshProUGUI>();
+        textmeshProUGUI.SetText("Hello World from Amanda"); 
+        }
+    }
+}
